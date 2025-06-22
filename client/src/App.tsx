4ca0148b61440d513da188905eae80e1d23623f9
yanni zhang <yanni.zhang.9710@icloud.com>
2025-06-22 13:03:13 +1000

    [#3] update page route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Expenses from './pages/Expenses'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Tailwind is Working!</h1>
-      <p className="text-gray-700 mt-2">Your CSS setup is now clean and ready.</p>
-    </div>
-    </>
+  <>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/expenses" element={<Expenses />} />
+    </Routes>
+  </>
   )
 }
 
---
+++ client/src/pages/Expenses.tsx
@@ -0,0 +1,8 @@
+export default function Expenses() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-indigo-600">Expenses</h1>
+      <p className="mt-4 text-gray-600">Track your spending here.</p>
+    </div>
+  );
+}
